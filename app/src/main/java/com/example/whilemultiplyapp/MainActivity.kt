f7548b2dfd3fdca890b699ec0121d1de4389ce1a
Liehan Els <lels@iie.ac.za>
2026-03-17 08:51:51 +0200

    Added a few lines of helper code

--- app/src/main/java/com/example/whilemultiplyapp/MainActivity.kt
+++ app/src/main/java/com/example/whilemultiplyapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.whilemultiplyapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val edtNumFirstNum = findViewById<EditText>(R.id.edtNumFirstNum)
+        val edtNumSecondNum = findViewById<EditText>(R.id.edtNumSecondNum)
+        val tvAnswer = findViewById<TextView>(R.id.tvAnswer)
+
+        btnCalculate.setOnClickListener {
+            var num1 = edtNumFirstNum.text.toString().toInt()
+            var num2 = edtNumSecondNum.text.toString().toInt()
+            var answer: Int = 0
+
+
+
+            tvAnswer.text = answer.toString()
         }
     }
 }
